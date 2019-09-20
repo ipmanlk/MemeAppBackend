e2443da9a686a52b5d9da4cb4b1bda66519af248
ipmanlk <io@navinda.xyz>
2019-09-21 00:49:35 +0530

    fix: hash is exposing on api

--- api/v1.0/dao.js
+++ api/v1.0/dao.js
@@ -4,9 +4,9 @@ const db = new sqlite3.Database('./data/memes.db');
 const getAll = (id) => {
     return new Promise((resolve, reject) => {
         if (id == null) {
-            sql = `SELECT * FROM memes ORDER BY id DESC LIMIT 10`;
+            sql = `SELECT id, url, date FROM memes ORDER BY id DESC LIMIT 10`;
         } else {
-            sql = `SELECT * FROM memes WEHRE id < ${id} ORDER BY id DESC LIMIT 10`;
+            sql = `SELECT id, url, date FROM memes WEHRE id < ${id} ORDER BY id DESC LIMIT 10`;
         }
         db.all(sql, [], (err, rows) => {
             if (err) {
