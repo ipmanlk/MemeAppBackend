84a72cc7e0cbcf7f8b7e97a228e61247686fd32c
ipmanlk <io@navinda.xyz>
2019-09-20 22:21:41 +0530

    root commit

---
+++ service/scrapper/dao.js
@@ -0,0 +1,40 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('../../data/memes.db');
+
+const check = (meme) => {
+    return new Promise((resolve, reject) => {
+        db.get(`SELECT * FROM memes WHERE hash = "${meme.hash}"`, [], (err, row) => {
+            if (row == undefined) resolve();
+            reject("Record already exist!");
+        });
+    })
+}
+
+const save = (meme) => {
+    check(meme).then(() => {
+        db.run(`INSERT INTO memes(hash, url, date) VALUES(?, ?, CURRENT_TIMESTAMP);`, [meme.hash, meme.url], function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            // get the last insert id
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+        });
+    }).catch(err => {
+        console.log(err);
+    })
+};
+
+const clean = () => {
+    db.run(`DELETE FROM memes;`, [], function (err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        console.log(`Row(s) deleted ${this.changes}`);
+    });
+};
+
+module.exports = {
+    save,
+    check
+}
+
---
+++ service/scrapper/scrapper.js
@@ -0,0 +1,76 @@
+const puppeteer = require('puppeteer');
+const request = require('request');
+const fs = require('fs');
+const sha256 = require('simple-sha256');
+const dao = require('./dao');
+
+const scrapeImages = async () => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    await page.goto('https://www.reddit.com/r/dankmemes/hot/');
+
+    await page.waitForSelector('img ', {
+        visible: true,
+    });
+
+    // Execute code in the DOM
+    const data = await page.evaluate(() => {
+        const images = document.querySelectorAll('.ImageBox-image');
+        const urls = Array.from(images).map(v => v.src);
+        return urls
+    });
+
+    await browser.close();
+    return data;
+}
+
+const download = (uri, hash) => {
+    dao.check({hash: hash}).then(() => {
+        let filepath = `./images/${hash}.jpg`;
+        request(uri).pipe(fs.createWriteStream(filepath)).on('close', () => {
+            upload(filepath).then(async (url) => {
+                console.log(url);
+                fs.unlinkSync(filepath);
+                dao.save({ url: url, hash: hash })
+            }).catch(err => {
+                fs.unlinkSync(filepath);
+                console.log(err);
+            })
+        });
+    }).catch(err => {
+        console.log(err);
+    })
+};
+
+const start = async () => {
+    let data = await scrapeImages();
+    data.forEach(async (url) => {
+        let imageHash = await sha256(url);
+        let hash = imageHash.substring(0, 9);
+        download(url, hash);
+    })
+}
+
+const upload = (filepath) => {
+    return new Promise((resolve, reject) => {
+        const req = request.post("https://0x0.st/", function (err, resp, body) {
+            if (err) {
+                reject(err);
+            } else {
+                if (/^(ftp|http|https):\/\/[^ "]+$/.test(body)) {
+                    resolve(body);
+                } else {
+                    reject(body);
+                }
+            }
+        });
+        const form = req.form();
+        form.append('file', fs.createReadStream(filepath));
+    })
+}
+
+// wait for some time
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+start();
