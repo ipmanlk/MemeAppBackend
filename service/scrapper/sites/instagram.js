14b2b4a3fdc8c8e53e434290e924da5c35b974d0
ipmanlk <io@navinda.xyz>
2019-11-10 22:16:45 +0530

    change: insta scrapper to night stalker

--- service/scrapper/sites/instagram.js
+++ service/scrapper/sites/instagram.js
@@ -1,66 +1,43 @@
-const puppeteer = require('puppeteer');
+const NightStalker = require("night-stalker");
 const dao = require(`${__dirname}/../dao.js`);
 const hashSum = require('hash-sum');
 
 const scrape = async () => {
-    const browser = await puppeteer.launch({
-        headless: true
-    });
-
     const pages = [
-        "https://www.instagram.com/_v_ery___s_orry_",
-        "https://www.instagram.com/memevinii",
-        "https://www.instagram.com/siri_soma",
-        "https://www.instagram.com/athalma_witharai",
-        "https://www.instagram.com/maha_hora",
-        "https://www.instagram.com/2020__a.l",
-        "https://www.instagram.com/kalawadda",
-        "https://www.instagram.com/athal_rusiya",
-        "https://www.instagram.com/athalma_witharai",
-        "https://www.instagram.com/kuppiya",
-        "https://www.instagram.com/chatarlanthaya",
-        "https://www.instagram.com/_v_ery___s_orry_",
-        "https://www.instagram.com/naifm.im",
-        "https://www.instagram.com/apisrilankanbro",
-        "https://www.instagram.com/a_m_d_a_",
-        "https://www.instagram.com/hitan_page",
-        "https://www.instagram.com/kuk_kuk_official",
-        "https://www.instagram.com/dmadu3924",
-        "https://www.instagram.com/athal_lokka_official"
+        "_v_ery___s_orry_",
+        "memevinii",
+        "siri_soma",
+        "athalma_witharai",
+        "maha_hora",
+        "2020__a.l",
+        "kalawadda",
+        "athal_rusiya",
+        "athalma_witharai",
+        "kuppiya",
+        "chatarlanthaya",
+        "_v_ery___s_orry_",
+        "naifm.im",
+        "apisrilankanbro",
+        "a_m_d_a_",
+        "hitan_page",
+        "kuk_kuk_official",
+        "dmadu3924",
+        "athal_lokka_official"
     ];
 
-    const page = await browser.newPage();
-    page.waitForNavigation({ timeout: 0 });
 
     for (let fbpage of pages) {
-        await page.goto(fbpage);
-        page.waitForNavigation({ timeout: 0 });
-        urls = await getImgUrls(page);
-        urls.reverse();
-        for (x = 0; x < urls.length; x++) {
-            let img = urls[x];
+        const balanar = await NightStalker.loadBrowser();
+        balanar.setUserName(fbpage);
+        const posts = await balanar.getPosts(5);
+
+        for (let post of posts) {
+            let img = post.media[0];
             let hash = hashSum(img);
             let source = fbpage;
             dao.save({ img, hash, source });
         }
     }
-
-    await browser.close();
-}
-
-const getImgUrls = async (page) => {
-    await page.waitForSelector('img ', {
-        visible: true,
-    });
-
-    // Execute code in the DOM
-    let data = await page.evaluate(() => {
-        const images = document.querySelectorAll('.KL4Bh img');
-        const urls = Array.from(images).map(v => v.src);
-        return urls
-    });
-
-    return data;
 }
 
 module.exports = {
